65e2d09fd633194caeb1594905873c931c5bb66c
charlescortez <charlesmph@gmail.com>
2019-08-14 11:24:47 -0400

    add express, add debugger script, add one task pull

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-const http = require('http');
+// const http = require('http');
+// replace with express
+const express = require('express');
 const Todo = require('./models/Todo');
 
-const server = http.createServer((req, res) => {
+// Create the server and call it "app"
+const app = express();
+// Create a variable for the port#
+const port = 3000;
+
+// const server = http.createServer((req, res) => {
+// Replace with app.get()
+app.get('/todos', (req, res) => {
+
+    // "debugger" keyword adds a programmatic breakpoint for the Chrome Dev Tools:
+
+    // debugger;
+
     console.log('you have a request!');
     const allTodos = Todo.getAll();
     allTodos
         .then((data) => {
             console.log("OMG IT'S DATAZZZ")
             console.log(data);
-            const dataJSON = JSON.stringify(data);
-            res.end(dataJSON);
+            // const dataJSON = JSON.stringify(data);
+            // res.end(dataJSON);
+            res.json(data);
         });
 });
-console.log(server);
-server.listen(3000);
+
+app.get('/todos/:taskId', (req, res) => {
+    const theId = parseInt(req.params.taskId, 10);
+    // convert 
+    const oneTodos = Todo.getOne(theId);
+    oneTodos.then((data) => {
+        console.log('specific task');
+        // console.log(req.params.taskId);
+        // const dataJSON = JSON.stringify(data);
+        // res.end(dataJSON);
+        res.json(data);
+    });
+});
+
+// console.log(req.params.taskId);
+// server.listen(3000);
+// Replace with app.listen(port, function())
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/Todo.js
+++ models/Todo.js
@@ -1,9 +1,3 @@
-// When you want one and only one, use the .one() method.
-// That way, if you don't find it, it triggers the catch().
-// This is better than doing an if/else inside your .then().
-// .one() whill throw an exception if it gets anything but one and only one result.
-
-
 const db = require('../db');
 
 function getAll() {
@@ -21,14 +15,16 @@ function getAll() {
 };
 
 function getOne(id) {
-    db.any(
-        `
-        select * from todos where id=$1
-        `, [id])
-    .then((data) => {
-        console.log('here is the data:');
-        console.log(data);
-    })
+    // When you want one and only one, use the .one() method.
+    // That way, if you don't find it, it triggers the catch().
+    // This is better than doing an if/else inside your .then().
+    // .one() whill throw an exception if it gets anything but one and only one result.
+    return db.one(
+            `select * from todos where id=$1`, [id])
+    // .then((data) => {
+    //     console.log('here is the data:');
+    //     console.log(data);
+    // })
     .catch((error) => {
         console.log('ruh roh....');
         console.log(error);
